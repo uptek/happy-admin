23490abfc159d5770faef2a8fe23b2e6d168a084
Shakil2022219 <shakilgi2222thub@gmail.com>
2020-01-15 01:20:00 +0500

    Refactor post sav shortcut

--- admin/js/happy-admin.js
+++ admin/js/happy-admin.js
@@ -28,6 +28,18 @@
 	 * Although scripts in the WordPress core, Plugins and Themes may be
 	 * practising this, we should strive to set a better example in our own work.
 	 */
-	
 
 })( jQuery );
+jQuery(document).keydown(function (event) {
+	// If Control or Command key is pressed and the S key is pressed
+	// run save function. 83 is the key code for S.
+	if ((event.ctrlKey || event.metaKey) && event.which == 83) {
+		// Save Function
+		jQuery('input[type="submit"]').trigger('click');
+		jQuery('.editor-post-publish-panel__toggle').trigger('click');
+		jQuery('.editor-post-publish-button').trigger('click');
+		
+		return false;
+	}
+}
+);
